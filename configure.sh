32d6013b7123dc0b47ca985aa138246502049c97
lionel untereiner <lionel.untereiner@inria.fr>
2015-10-08 18:24:28 +0200

    improvement of configure.sh and master CMakeList

--- configure.sh
+++ configure.sh
@@ -11,24 +11,45 @@ else
 	exit 1
 fi
 
+usage="usage : ./configure.sh build-platform"
 
-os="Linux64-gcc"
+
+# Check the current OS
+os="$1"
+if [ -z "$os"] #if os is empty
+	then 
+		os=`uname -a`
+fi		
+
+case "$os" in
+	Linux*x86_64*)
+		os=Linux64-gcc;;
+	Linux*amd64*)
+		os=Linux64-gcc;;
+	Darwin)
+		os=Darwin-clang;;
+	*)
+		echo "Error: OS not supported: $os"			
+		exit 1;;
+esac	
+
+# Generate the Makefiles
 for config in Release Debug
 do
+	platform=$os-$config
 	echo
 	echo "-------------- Checking makefiles for $config mode --------------"
 	echo
 
-	mkdir -p build/$os-$config
-	(cd build/$os-$config; cmake -DCMAKE_BUILD_TYPE:STRING=$config -DCGoGN_PLATFORM:STRING=$os ../../)
+	mkdir -p build/$platform
+	(cd build/$platform; cmake -DCMAKE_BUILD_TYPE:STRING=$config -DCGoGN_PLATFORM:STRING=$os ../../)
 done
 
 echo
 echo "-------------- CGoGN build configured --------------"
 echo
+echo "to build CGoGN:
+- go to build/$os-Release or build/$os-Debug
+- and 'make' or 'cmake --build .'"
 
-cat << EOF
-to build CGoGN:
-	- go to build/$os-Release or build/$os-Debug
-	- and 'make' or 'cmake --build .'
-EOF
+exit $?
