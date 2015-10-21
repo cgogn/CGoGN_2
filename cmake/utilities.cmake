3eb734928ba2eccd98d60be92ee34f9aa49740f6
Sylvain Thery <thery@unistra.fr>
2015-10-21 17:55:49 +0200

    auto build type

--- cmake/utilities.cmake
+++ cmake/utilities.cmake
@@ -32,3 +32,20 @@ function(aux_source_directories var folder)
     #message("\nDEBUG: aux_source_directories: current_source_dir=${CMAKE_CURRENT_SOURCE_DIR} dirs=${ARGN}):\n${sources}\n")
     set(${var} ${${var}} ${sources} PARENT_SCOPE)
 endfunction()
+
+#!
+# @brief automatic deduction of CMAKE_BUILD_TYPE from CMAKE_CURRENT_BINARY_DIR
+# @details
+# if CMAKE_CURRENT_BINARY_DIR end with Debug/debug/DEBUG, set Debug mode else Release mode.
+#
+function(setBuildType)
+IF (NOT (${CMAKE_BUILD_TYPE} MATCHES "Debug|Release"))
+	IF (${CMAKE_CURRENT_BINARY_DIR} MATCHES "(.*)Debug|(.*)debug|(.*)DEBUG")
+		SET(CMAKE_BUILD_TYPE "Debug" PARENT_SCOPE)
+	ELSE()
+		SET(CMAKE_BUILD_TYPE "Release" PARENT_SCOPE)
+	ENDIF()
+ENDIF()
+endfunction(setBuildType)
+
+
