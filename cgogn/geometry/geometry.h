901e7d9eb08f8208be05a7950468a82ec4752627
David Cazier/develop <david.cazier@unistra.fr>
2015-11-27 18:30:58 +0100

    Essais d'utilisation de Eigen 3 versions suivant le degrée d'inclusion souhaité
    Veuillez saisir le message de validation pour vos modifications. Les lignes

--- cgogn/geometry/geometry.h
+++ cgogn/geometry/geometry.h
@@ -1,6 +1,40 @@
 #ifndef GEOMETRY_GEOMETRY_H_
 #define GEOMETRY_GEOMETRY_H_
 
+#include <Eigen/Dense>
 
+namespace cgogn {
+
+
+// linear interpolation between 2 points
+template <typename SCALAR, unsigned int DIM>
+Eigen::Matrix<SCALAR,DIM,1> lerp(const Eigen::Matrix<SCALAR,DIM,1>& v1,
+								 const Eigen::Matrix<SCALAR,DIM,1>& v2,
+								 SCALAR s)
+{
+	return (1.0 - s) * v1 + s * v2 ;
+}
+
+template<typename SCALAR, unsigned int DIM>
+using Vector = Eigen::Matrix<SCALAR, DIM, 1>;
+
+// linear interpolation between 2 points version 2
+template <typename SCALAR, unsigned int DIM>
+Vector<SCALAR,DIM> lerp2(const Vector<SCALAR,DIM>& v1,
+						 const Vector<SCALAR,DIM>& v2,
+						 SCALAR s)
+{
+	return (1.0 - s) * v1 + s * v2 ;
+}
+
+
+// weighted barycenter of 2 points
+template <typename VECT>
+VECT barycenter(const VECT& v1, const VECT& v2, typename VECT::Scalar a, typename VECT::Scalar b)
+{
+	return a * v1 + b * v2 ;
+}
+
+} // namespace cgogn
 
 #endif // GEOMETRY_GEOMETRY_H_
--- test/geometry/test_geometry.cpp
+++ test/geometry/test_geometry.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <geometry/geometry.h>
-#include <Eigen/Dense>
 
-using namespace Eigen;
-using namespace std;
 
 int main()
 {
-  Matrix3d m = Matrix3d::Random();
-  m = (m + Matrix3d::Constant(1.2)) * 50;
-  cout << "m =" << endl << m << endl;
-  Vector3d v(1,2,3);
+  Eigen::Matrix3d m = Eigen::Matrix3d::Random();
+  m = (m + Eigen::Matrix3d::Constant(1.2)) * 50;
 
-  cout << "m * v =" << endl << m * v << endl;
+  Eigen::Matrix<double,3,1> u(1,2,3);
+
+  cgogn::Vector<double,3> v(1,2,3);
+
+  Eigen::Vector3d w;
+
+  w = cgogn::lerp<double,3>(u,v,0.5);
+  std::cout << "w1 =" << w << std::endl;
+
+  w = cgogn::barycenter<Eigen::Vector3d>(u,v,0.5,0.5);
+  std::cout << "w2 =" << w << std::endl;
+
+  w = cgogn::lerp2<double,3>(u,v,0.5);
+  std::cout << "w3 =" << w << std::endl;
 }
