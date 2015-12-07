26793fd6db1083adb9cdf5b14653489878894586
Etienne Schmitt <etienne.schmitt@inria.fr>
2015-12-07 09:34:26 +0100

    fixed compilation under MSVC2013
    Signed-off-by: Etienne Schmitt <etienne.schmitt@inria.fr>

--- cgogn/core/basic/name_types.cpp
+++ cgogn/core/basic/name_types.cpp
@@ -21,43 +21,56 @@
 *                                                                              *
 *******************************************************************************/
 
-
+#define CGOGN_CORE_DLL_EXPORT
 #include <core/basic/name_types.h>
 
 namespace cgogn
 {
 
+template <>
+CGOGN_CORE_API std::string name_of_type(const bool& /*v*/) { return "bool"; }
 
+template <>
+CGOGN_CORE_API std::string name_of_type(const char& /*v*/) { return "char"; }
 
-template <>  std::string name_of_type(const bool& /*v*/) { return "bool"; }
+template <>
+CGOGN_CORE_API std::string name_of_type(const short& /*v*/) { return "short"; }
 
-template <>  std::string name_of_type(const char& /*v*/) { return "char"; }
+template <>
+CGOGN_CORE_API std::string name_of_type(const int& /*v*/) { return "int"; }
 
-template <>  std::string name_of_type(const short& /*v*/) { return "short"; }
+template <>
+CGOGN_CORE_API std::string name_of_type(const long& /*v*/) { return "long"; }
 
-template <>  std::string name_of_type(const int& /*v*/) { return "int"; }
+template <>
+CGOGN_CORE_API std::string name_of_type(const long long& /*v*/) { return "long long"; }
 
-template <>  std::string name_of_type(const long& /*v*/) { return "long"; }
+template <>
+CGOGN_CORE_API std::string name_of_type(const signed char& /*v*/) { return "signed char"; }
 
-template <>  std::string name_of_type(const long long& /*v*/) { return "long long"; }
+template <>
+CGOGN_CORE_API std::string name_of_type(const unsigned char& /*v*/) { return "unsigned char"; }
 
-template <>  std::string name_of_type(const signed char& /*v*/) { return "signed char"; }
+template <>
+CGOGN_CORE_API std::string name_of_type(const unsigned short& /*v*/) { return "unsigned short"; }
 
-template <>  std::string name_of_type(const unsigned char& /*v*/) { return "unsigned char"; }
+template <>
+CGOGN_CORE_API std::string name_of_type(const unsigned int& /*v*/) { return "unsigned int"; }
 
-template <>  std::string name_of_type(const unsigned short& /*v*/) { return "unsigned short"; }
+template <>
+CGOGN_CORE_API std::string name_of_type(const unsigned long& /*v*/) { return "unsigned long"; }
 
-template <>  std::string name_of_type(const unsigned int& /*v*/) { return "unsigned int"; }
+template <>
+CGOGN_CORE_API std::string name_of_type(const unsigned long long& /*v*/) { return "unsigned long long"; }
 
-template <>  std::string name_of_type(const unsigned long& /*v*/) { return "unsigned long"; }
+template <>
+CGOGN_CORE_API std::string name_of_type(const float& /*v*/) { return "float"; }
 
-template <>  std::string name_of_type(const unsigned long long& /*v*/) { return "unsigned long long"; }
+template <>
+CGOGN_CORE_API std::string name_of_type(const double& /*v*/) { return "double"; }
 
-template <>  std::string name_of_type(const float& /*v*/) { return "float"; }
-
-template <>  std::string name_of_type(const double& /*v*/) { return "double"; }
-
-template <>  std::string name_of_type(const std::string& /*v*/) { return "std::string"; }
+template <>
+CGOGN_CORE_API std::string name_of_type(const std::string& /*v*/) { return "std::string"; }
 
 
 } // namespace cgogn
