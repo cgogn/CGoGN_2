f70f5d7d87ed856c9309a1884ce435e8f44b1ab8
Étienne Schmitt <etienne.schmitt@inria.fr>
2016-01-05 11:38:11 +0100

    Removed the import from CMap2_T and added them to SurfaceImport
    Signed-off-by: Étienne Schmitt <etienne.schmitt@inria.fr>

--- cgogn/io/surface_import.h
+++ cgogn/io/surface_import.h
@@ -32,288 +32,393 @@ CGOGN_PRAGMA_EIGEN_REMOVE_WARNINGS_ON
 #include <Eigen/Dense>
 CGOGN_PRAGMA_EIGEN_REMOVE_WARNINGS_OFF
 
+#include <core/map/cmap2.h>
 namespace cgogn
 {
 
-enum SurfaceFileType
-{
-	SurfaceFileType_UNKNOWN = 0,
-	SurfaceFileType_OFF,
-	SurfaceFileType_OBJ
-};
+    enum SurfaceFileType
+    {
+        SurfaceFileType_UNKNOWN = 0,
+        SurfaceFileType_OFF,
+        SurfaceFileType_OBJ
+    };
 
-inline SurfaceFileType get_file_type(const std::string& filename)
-{
-	if (filename.rfind(".off") != std::string::npos || filename.rfind(".OFF") != std::string::npos)
-		return SurfaceFileType_OFF;
-	if (filename.rfind(".obj") != std::string::npos || filename.rfind(".OBJ") != std::string::npos)
-		return SurfaceFileType_OBJ;
-	return SurfaceFileType_UNKNOWN;
-}
+    inline SurfaceFileType get_file_type(const std::string& filename)
+    {
+        if (filename.rfind(".off") != std::string::npos || filename.rfind(".OFF") != std::string::npos)
+            return SurfaceFileType_OFF;
+        if (filename.rfind(".obj") != std::string::npos || filename.rfind(".OBJ") != std::string::npos)
+            return SurfaceFileType_OBJ;
+        return SurfaceFileType_UNKNOWN;
+    }
 
-template <typename DATA_TRAITS>
-class SurfaceImport
-{
-public:
-	using Self = SurfaceImport<DATA_TRAITS>;
-	using UnsignedChunkArray = ChunkArrayContainer<DATA_TRAITS::CHUNK_SIZE, unsigned int>;
+    template <typename MAP>
+    class SurfaceImport
+    {
+    public:
+        using Self = SurfaceImport<MAP>;
+        using Map = MAP;
 
-	unsigned int nb_vertices_;
-	unsigned int nb_edges_;
-	unsigned int nb_faces_;
+        template<typename T>
+        using ChunkArray =  typename Map::template ChunkArray<T>;
+        using ChunkArrayContainer = typename Map::template ChunkArrayContainer<unsigned int>;
 
-	std::vector<unsigned short> faces_nb_edges_;
-	std::vector<unsigned int> faces_vertex_indices_;
+        template<typename T, Orbit ORBIT>
+        using AttributeHandler = typename Map::template AttributeHandler<T, ORBIT>;
+        template<typename T>
+        using VertexAttributeHandler = AttributeHandler<T, Map::VERTEX>;
+        using Vec3 = typename Map::DataTraits::Vec3;
+        unsigned int nb_vertices_;
+        unsigned int nb_edges_;
+        unsigned int nb_faces_;
 
-	UnsignedChunkArray vertex_attributes_;
+        std::vector<unsigned short> faces_nb_edges_;
+        std::vector<unsigned int> faces_vertex_indices_;
 
-	SurfaceImport() :
-		nb_vertices_(0u)
-	  ,nb_edges_(0u)
-	  ,nb_faces_(0u)
-	  ,faces_nb_edges_()
-	  ,faces_vertex_indices_()
-	{}
+        ChunkArrayContainer vertex_attributes_;
 
-	~SurfaceImport()
-	{}
+        SurfaceImport() :
+            nb_vertices_(0u)
+          ,nb_edges_(0u)
+          ,nb_faces_(0u)
+          ,faces_nb_edges_()
+          ,faces_vertex_indices_()
+        {}
 
-	SurfaceImport(const Self&) = delete;
-	SurfaceImport(Self&&) = delete;
-	Self& operator=(const Self&) = delete;
-	Self& operator=(Self&&) = delete;
+        ~SurfaceImport()
+        {}
 
-	void clear()
-	{
-		nb_vertices_ = 0;
-		nb_edges_ = 0;
-		nb_faces_ = 0;
-		faces_nb_edges_.clear();
-		faces_vertex_indices_.clear();
-	}
+        SurfaceImport(const Self&) = delete;
+        SurfaceImport(Self&&) = delete;
+        Self& operator=(const Self&) = delete;
+        Self& operator=(Self&&) = delete;
 
-	bool import_file(const std::string& filename)
-	{
-		return import_file(filename, get_file_type(filename));
-	}
+        void clear()
+        {
+            nb_vertices_ = 0;
+            nb_edges_ = 0;
+            nb_faces_ = 0;
+            faces_nb_edges_.clear();
+            faces_vertex_indices_.clear();
+        }
 
-	bool import_file(const std::string& filename, SurfaceFileType type)
-	{
-		std::ifstream fp(filename.c_str(), std::ios::in);
-		if (!fp.good())
-		{
-			std::cout << "Unable to open file " << filename << std::endl;
-			return false;
-		}
+        bool import_file(const std::string& filename)
+        {
+            return import_file(filename, get_file_type(filename));
+        }
 
-		clear();
+        bool import_file(const std::string& filename, SurfaceFileType type)
+        {
+            std::ifstream fp(filename.c_str(), std::ios::in);
+            if (!fp.good())
+            {
+                std::cout << "Unable to open file " << filename << std::endl;
+                return false;
+            }
 
-		bool result = false;
-		switch (type)
-		{
-			case SurfaceFileType_UNKNOWN :
-				std::cout << "Unknown file type " << filename << std::endl;
-				result = false;
-				break;
-			case SurfaceFileType_OFF :
-				result = import_OFF(fp);
-				break;
-			case SurfaceFileType_OBJ :
-				result = import_OBJ(fp);
-				break;
-		}
+            clear();
 
-		fp.close();
+            bool result = false;
+            switch (type)
+            {
+                case SurfaceFileType_UNKNOWN :
+                    std::cout << "Unknown file type " << filename << std::endl;
+                    result = false;
+                    break;
+                case SurfaceFileType_OFF :
+                    result = import_OFF(fp);
+                    break;
+                case SurfaceFileType_OBJ :
+                    result = import_OBJ(fp);
+                    break;
+            }
 
-		return result;
-	}
+            fp.close();
 
-protected:
+            return result;
+        }
 
-	bool import_OFF(std::ifstream& fp)
-	{
-		typedef Eigen::Vector3d VEC3;
+        void createMap(Map& map)
+        {
+            const Orbit VERTEX = Map::VERTEX;
+            map.clear_and_remove();
 
-		std::string line;
+            map.template create_embedding<VERTEX>();
+            map.attributes_[VERTEX].swap(this->vertex_attributes_);
 
-		// read OFF header
-		std::getline(fp, line);
-		if (line.rfind("OFF") == std::string::npos)
-		{
-			std::cout << "Problem reading off file: not an off file" << std::endl;
-			std::cout << line << std::endl;
-			return false;
-		}
+            VertexAttributeHandler<std::vector<Dart>> darts_per_vertex =
+                    map.template add_attribute<std::vector<Dart>, VERTEX>("darts_per_vertex");
 
-		// read number of vertices, edges, faces
-		do
-		{
-			std::getline(fp, line);
-		} while (line.size() == 0);
-		{ // limit scope of oss
-			std::stringstream oss(line);
+            unsigned int faces_vertex_index = 0;
+            std::vector<unsigned int> vertices_buffer;
+            vertices_buffer.reserve(16);
 
-			oss >> nb_vertices_;
-			oss >> nb_faces_;
-			oss >> nb_edges_;
-		}
-		ChunkArray<DATA_TRAITS::CHUNK_SIZE, VEC3>* position =
-			vertex_attributes_.template add_attribute<VEC3>("position");
+            for (unsigned int i = 0; i < this->nb_faces_; ++i)
+            {
+                unsigned short nbe = this->faces_nb_edges_[i];
 
-		// read vertices position
-		std::vector<unsigned int> vertices_id;
-		vertices_id.reserve(nb_vertices_);
+                vertices_buffer.clear();
+                unsigned int prev = std::numeric_limits<unsigned int>::max();
 
-		for (unsigned int i = 0; i < nb_vertices_; ++i)
-		{
-			do
-			{
-				std::getline(fp, line);
-			} while (line.size() == 0);
+                for (unsigned int j = 0; j < nbe; ++j)
+                {
+                    unsigned int idx = this->faces_vertex_indices_[faces_vertex_index++];
+                    if (idx != prev)
+                    {
+                        prev = idx;
+                        vertices_buffer.push_back(idx);
+                    }
+                }
+                if (vertices_buffer.front() == vertices_buffer.back())
+                    vertices_buffer.pop_back();
 
-			std::stringstream oss(line);
+                nbe = static_cast<unsigned short>(vertices_buffer.size());
+                if (nbe > 2)
+                {
+                    Dart d = map.Map::Inherit::add_face_topo(nbe);
+                    for (unsigned int j = 0; j < nbe; ++j)
+                    {
+                        unsigned int vertex_index = vertices_buffer[j];
+                        map.template init_embedding<VERTEX>(d, vertex_index);
+                        darts_per_vertex[vertex_index].push_back(d);
+                        d = map.phi1(d);
+                    }
+                }
+            }
 
-			float x, y, z;
-			oss >> x;
-			oss >> y;
-			oss >> z;
+            bool need_vertex_unicity_check = false;
+            unsigned int nb_boundary_edges = 0;
 
-			VEC3 pos(x, y, z);
+            for (Dart d : map)
+            {
+                if (map.phi2(d) == d)
+                {
+                    unsigned int vertex_index = map.template get_embedding<VERTEX>(d);
 
-			unsigned int vertex_id = vertex_attributes_.template insert_lines<1>();
-			(*position)[vertex_id] = pos;
+                    std::vector<Dart>& next_vertex_darts = darts_per_vertex[map.phi1(d)];
+                    bool phi2_found = false;
+                    bool first_OK = true;
 
-			vertices_id.push_back(vertex_id);
-		}
+                    for (auto it = next_vertex_darts.begin();
+                         it != next_vertex_darts.end() && !phi2_found;
+                         ++it)
+                    {
+                        if (map.template get_embedding<VERTEX>(map.phi1(*it)) == vertex_index)
+                        {
+                            if (map.phi2(*it) == *it)
+                            {
+                                map.phi2_sew(d, *it);
+                                phi2_found = true;
+                            }
+                            else
+                            {
+                                first_OK = false;
+                            }
+                        }
+                    }
 
-		// read faces (vertex indices)
-		faces_nb_edges_.reserve(nb_faces_);
-		faces_vertex_indices_.reserve(nb_vertices_ * 8);
-		for (unsigned int i = 0; i < nb_faces_; ++i)
-		{
-			do
-			{
-				std::getline(fp, line);
-			} while (line.size() == 0);
+                    if (!phi2_found)
+                        ++nb_boundary_edges;
 
-			std::stringstream oss(line);
+                    if (!first_OK)
+                        need_vertex_unicity_check = true;
+                }
+            }
 
-			unsigned short n;
-			oss >> n;
-			faces_nb_edges_.push_back(n);
-			for (unsigned int j = 0; j < n; ++j)
-			{
-				unsigned int index;
-				oss >> index;
-				faces_vertex_indices_.push_back(vertices_id[index]);
-			}
-		}
+            if (nb_boundary_edges > 0)
+                map.close_map();
 
-		return true;
-	}
+            if (need_vertex_unicity_check)
+                map.template unique_orbit_embedding<VERTEX>();
 
-	bool import_OBJ(std::ifstream& fp)
-	{
-		typedef Eigen::Vector3d VEC3;
+            map.remove_attribute(darts_per_vertex);
+        }
 
-		ChunkArray<DATA_TRAITS::CHUNK_SIZE, VEC3>* position =
-			vertex_attributes_.template add_attribute<VEC3>("position");
 
-		std::string line, tag;
+    protected:
 
-		do
-		{
-			fp >> tag;
-			std::getline(fp, line);
-		} while (tag != std::string("v"));
+        bool import_OFF(std::ifstream& fp)
+        {
 
-		// lecture des sommets
-		std::vector<unsigned int> vertices_id;
-		vertices_id.reserve(102400);
+            std::string line;
 
-		unsigned int i = 0;
-		do
-		{
-			if (tag == std::string("v"))
-			{
-				std::stringstream oss(line);
+            // read OFF header
+            std::getline(fp, line);
+            if (line.rfind("OFF") == std::string::npos)
+            {
+                std::cout << "Problem reading off file: not an off file" << std::endl;
+                std::cout << line << std::endl;
+                return false;
+            }
 
-				float x, y, z;
-				oss >> x;
-				oss >> y;
-				oss >> z;
+            // read number of vertices, edges, faces
+            do
+            {
+                std::getline(fp, line);
+            } while (line.size() == 0);
+            { // limit scope of oss
+                std::stringstream oss(line);
 
-				VEC3 pos(x, y, z);
+                oss >> nb_vertices_;
+                oss >> nb_faces_;
+                oss >> nb_edges_;
+            }
+            ChunkArray<Vec3>* position =
+                    vertex_attributes_.template add_attribute<Vec3>("position");
 
-				unsigned int vertex_id = vertex_attributes_.template insert_lines<1>();
-				(*position)[vertex_id] = pos;
+            // read vertices position
+            std::vector<unsigned int> vertices_id;
+            vertices_id.reserve(nb_vertices_);
 
-				vertices_id.push_back(vertex_id);
-				i++;
-			}
+            for (unsigned int i = 0; i < nb_vertices_; ++i)
+            {
+                do
+                {
+                    std::getline(fp, line);
+                } while (line.size() == 0);
 
-			fp >> tag;
-			std::getline(fp, line);
-		} while (!fp.eof());
+                std::stringstream oss(line);
 
-		nb_vertices_ = static_cast<unsigned int>(vertices_id.size());
+                double x, y, z;
+                oss >> x;
+                oss >> y;
+                oss >> z;
 
-		fp.clear();
-		fp.seekg(0, std::ios::beg);
+                Vec3 pos{x, y, z};
 
-		do
-		{
-			fp >> tag;
-			std::getline(fp, line);
-		} while (tag != std::string("f"));
+                unsigned int vertex_id = vertex_attributes_.template insert_lines<1>();
+                (*position)[vertex_id] = pos;
 
-		faces_nb_edges_.reserve(vertices_id.size() * 2);
-		faces_vertex_indices_.reserve(vertices_id.size() * 8);
+                vertices_id.push_back(vertex_id);
+            }
 
-		std::vector<unsigned int> table;
-		table.reserve(64);
-		do
-		{
-			if (tag == std::string("f")) // lecture d'une face
-			{
-				std::stringstream oss(line);
+            // read faces (vertex indices)
+            faces_nb_edges_.reserve(nb_faces_);
+            faces_vertex_indices_.reserve(nb_vertices_ * 8);
+            for (unsigned int i = 0; i < nb_faces_; ++i)
+            {
+                do
+                {
+                    std::getline(fp, line);
+                } while (line.size() == 0);
 
-				table.clear();
-				while (!oss.eof())  // lecture de tous les indices
-				{
-					std::string str;
-					oss >> str;
+                std::stringstream oss(line);
 
-					unsigned int ind = 0;
+                unsigned short n;
+                oss >> n;
+                faces_nb_edges_.push_back(n);
+                for (unsigned int j = 0; j < n; ++j)
+                {
+                    unsigned int index;
+                    oss >> index;
+                    faces_vertex_indices_.push_back(vertices_id[index]);
+                }
+            }
 
-					while ((ind < str.length()) && (str[ind] != '/'))
-						ind++;
+            return true;
+        }
 
-					if (ind > 0)
-					{
-						unsigned int index;
-						std::stringstream iss(str.substr(0, ind));
-						iss >> index;
-						table.push_back(index);
-					}
-				}
+        bool import_OBJ(std::ifstream& fp)
+        {
 
-				unsigned int n = static_cast<unsigned int>(table.size());
-				faces_nb_edges_.push_back(static_cast<unsigned short>(n));
-				for (unsigned int j = 0; j < n; ++j)
-				{
-					unsigned int index = table[j] - 1; // indices start at 1
-					faces_vertex_indices_.push_back(vertices_id[index]);
-				}
-				nb_faces_++;
-			}
-			fp >> tag;
-			std::getline(fp, line);
-		 } while (!fp.eof());
+            ChunkArray<Vec3>* position =
+                    vertex_attributes_.template add_attribute<Vec3>("position");
 
-		return true;
-	}
-};
+            std::string line, tag;
+
+            do
+            {
+                fp >> tag;
+                std::getline(fp, line);
+            } while (tag != std::string("v"));
+
+            // lecture des sommets
+            std::vector<unsigned int> vertices_id;
+            vertices_id.reserve(102400);
+
+            unsigned int i = 0;
+            do
+            {
+                if (tag == std::string("v"))
+                {
+                    std::stringstream oss(line);
+
+                    double x, y, z;
+                    oss >> x;
+                    oss >> y;
+                    oss >> z;
+
+                    Vec3 pos{x, y, z};
+
+                    unsigned int vertex_id = vertex_attributes_.template insert_lines<1>();
+                    (*position)[vertex_id] = pos;
+
+                    vertices_id.push_back(vertex_id);
+                    i++;
+                }
+
+                fp >> tag;
+                std::getline(fp, line);
+            } while (!fp.eof());
+
+            nb_vertices_ = static_cast<unsigned int>(vertices_id.size());
+
+            fp.clear();
+            fp.seekg(0, std::ios::beg);
+
+            do
+            {
+                fp >> tag;
+                std::getline(fp, line);
+            } while (tag != std::string("f"));
+
+            faces_nb_edges_.reserve(vertices_id.size() * 2);
+            faces_vertex_indices_.reserve(vertices_id.size() * 8);
+
+            std::vector<unsigned int> table;
+            table.reserve(64);
+            do
+            {
+                if (tag == std::string("f")) // lecture d'une face
+                {
+                    std::stringstream oss(line);
+
+                    table.clear();
+                    while (!oss.eof())  // lecture de tous les indices
+                    {
+                        std::string str;
+                        oss >> str;
+
+                        unsigned int ind = 0;
+
+                        while ((ind < str.length()) && (str[ind] != '/'))
+                            ind++;
+
+                        if (ind > 0)
+                        {
+                            unsigned int index;
+                            std::stringstream iss(str.substr(0, ind));
+                            iss >> index;
+                            table.push_back(index);
+                        }
+                    }
+
+                    unsigned int n = static_cast<unsigned int>(table.size());
+                    faces_nb_edges_.push_back(static_cast<unsigned short>(n));
+                    for (unsigned int j = 0; j < n; ++j)
+                    {
+                        unsigned int index = table[j] - 1; // indices start at 1
+                        faces_vertex_indices_.push_back(vertices_id[index]);
+                    }
+                    nb_faces_++;
+                }
+                fp >> tag;
+                std::getline(fp, line);
+            } while (!fp.eof());
+
+            return true;
+        }
+    };
 
 } // namespace cgogn
 
