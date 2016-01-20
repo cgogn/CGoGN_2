0b87babf42fc431f42421baf843dbf85c01d1097
Étienne Schmitt <etienne.schmitt@inria.fr>
2016-01-20 19:15:15 +0100

    added tests for projection and orientation.
    Signed-off-by: Étienne Schmitt <etienne.schmitt@inria.fr>

--- cgogn/geometry/tests/geometry/test_plane_3d.cpp
+++ cgogn/geometry/tests/geometry/test_plane_3d.cpp
@@ -12,3 +12,44 @@ TEST(Plane3D_TEST, NameOfType)
 	EXPECT_EQ(cgogn::name_of_type(Plane3D1(Vec1(),0.)), "geometry::Plane3D<std::array<double,3>>");
 	EXPECT_EQ(cgogn::name_of_type(Plane3D2(Vec2(),0.)), "geometry::Plane3D<Eigen::Vector3d>");
 }
+
+TEST(Plane3D_TEST, Project)
+{
+	{
+		Plane3D1 plane(Vec1{4,0,0}, Vec1{0,0,0});
+		Vec1 p{5,8,12};
+		plane.project(p);
+		EXPECT_EQ(p[0], 0.);
+		EXPECT_EQ(p[1], 8.);
+		EXPECT_EQ(p[2], 12.);
+	}
+	{
+		Plane3D2 plane(Vec2{4,0,0}, Vec2{0,0,0});
+		Vec2 p{5,8,12};
+		plane.project(p);
+		EXPECT_EQ(p[0], 0.);
+		EXPECT_EQ(p[1], 8.);
+		EXPECT_EQ(p[2], 12.);
+	}
+}
+TEST(Plane3D_TEST, Orient)
+{
+	{
+		Plane3D1 plane(Vec1{0,0,15}, Vec1{0,0,0});
+		Vec1 p1{546854,864,12};
+		Vec1 p2{-5,886486,-12};
+		Vec1 p3{44552,7,0};
+		EXPECT_EQ(plane.orient(p1), cgogn::geometry::Orientation3D::OVER);
+		EXPECT_EQ(plane.orient(p2), cgogn::geometry::Orientation3D::UNDER);
+		EXPECT_EQ(plane.orient(p3), cgogn::geometry::Orientation3D::ON);
+	}
+	{
+		Plane3D2 plane(Vec2{0,0,15}, Vec2{0,0,0});
+		Vec2 p1{546854,864,12};
+		Vec2 p2{-5,886486,-12};
+		Vec2 p3{44552,7,0};
+		EXPECT_EQ(plane.orient(p1), cgogn::geometry::Orientation3D::OVER);
+		EXPECT_EQ(plane.orient(p2), cgogn::geometry::Orientation3D::UNDER);
+		EXPECT_EQ(plane.orient(p3), cgogn::geometry::Orientation3D::ON);
+	}
+}
