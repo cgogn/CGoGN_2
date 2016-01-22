eb85bb032dbda1c926b14a1b19f4129567c253dd
Étienne Schmitt <etienne.schmitt@inria.fr>
2016-01-22 14:49:49 +0100

    Added Vec_T class and updated geometry_traits and tests.
    Signed-off-by: Étienne Schmitt <etienne.schmitt@inria.fr>

--- cgogn/geometry/tests/geometry/test_plane_3d.cpp
+++ cgogn/geometry/tests/geometry/test_plane_3d.cpp
@@ -1,31 +1,34 @@
 #include <geometry/plane_3d.h>
 #include <gtest/gtest.h>
 #include <iostream>
+CGOGN_PRAGMA_EIGEN_REMOVE_WARNINGS_ON
+#include <Eigen/Dense>
+CGOGN_PRAGMA_EIGEN_REMOVE_WARNINGS_OFF
 
-using Vec1 = std::array<double,3>;
-using Vec2 = Eigen::Vector3d;
-using Plane3D1 = cgogn::geometry::Plane3D<Vec1>;
-using Plane3D2 = cgogn::geometry::Plane3D<Vec2>;
+using StdArray = cgogn::geometry::Vec_T<std::array<double,3>>;
+using EigenVec3d = Eigen::Vector3d;
+using Plane3D_Array = cgogn::geometry::Plane3D<StdArray>;
+using Plane3D_Eigen = cgogn::geometry::Plane3D<EigenVec3d>;
 
 TEST(Plane3D_TEST, NameOfType)
 {
-	EXPECT_EQ(cgogn::name_of_type(Plane3D1(Vec1(),0.)), "geometry::Plane3D<std::array<double,3>>");
-	EXPECT_EQ(cgogn::name_of_type(Plane3D2(Vec2(),0.)), "geometry::Plane3D<Eigen::Vector3d>");
+	EXPECT_EQ(cgogn::name_of_type(Plane3D_Array(StdArray(),0.)), "geometry::Plane3D<geometry::Vec_T<std::array<double,3>>>");
+	EXPECT_EQ(cgogn::name_of_type(Plane3D_Eigen(EigenVec3d(),0.)), "geometry::Plane3D<Eigen::Vector3d>");
 }
 
 TEST(Plane3D_TEST, Project)
 {
 	{
-		Plane3D1 plane(Vec1{4,0,0}, Vec1{0,0,0});
-		Vec1 p{5,8,12};
+		Plane3D_Array plane(StdArray{4,0,0}, StdArray{0,0,0});
+		StdArray p{5,8,12};
 		plane.project(p);
 		EXPECT_EQ(p[0], 0.);
 		EXPECT_EQ(p[1], 8.);
 		EXPECT_EQ(p[2], 12.);
 	}
 	{
-		Plane3D2 plane(Vec2{4,0,0}, Vec2{0,0,0});
-		Vec2 p{5,8,12};
+		Plane3D_Eigen plane(EigenVec3d{4,0,0}, EigenVec3d{0,0,0});
+		EigenVec3d p{5,8,12};
 		plane.project(p);
 		EXPECT_EQ(p[0], 0.);
 		EXPECT_EQ(p[1], 8.);
@@ -35,19 +38,19 @@ TEST(Plane3D_TEST, Project)
 TEST(Plane3D_TEST, Orient)
 {
 	{
-		Plane3D1 plane(Vec1{0,0,15}, Vec1{0,0,0});
-		Vec1 p1{546854,864,12};
-		Vec1 p2{-5,886486,-12};
-		Vec1 p3{44552,7,0};
+		Plane3D_Array plane(StdArray{0,0,15}, StdArray{0,0,0});
+		StdArray p1{546854,864,12};
+		StdArray p2{-5,886486,-12};
+		StdArray p3{44552,7,0};
 		EXPECT_EQ(plane.orient(p1), cgogn::geometry::Orientation3D::OVER);
 		EXPECT_EQ(plane.orient(p2), cgogn::geometry::Orientation3D::UNDER);
 		EXPECT_EQ(plane.orient(p3), cgogn::geometry::Orientation3D::ON);
 	}
 	{
-		Plane3D2 plane(Vec2{0,0,15}, Vec2{0,0,0});
-		Vec2 p1{546854,864,12};
-		Vec2 p2{-5,886486,-12};
-		Vec2 p3{44552,7,0};
+		Plane3D_Eigen plane(EigenVec3d{0,0,15}, EigenVec3d{0,0,0});
+		EigenVec3d p1{546854,864,12};
+		EigenVec3d p2{-5,886486,-12};
+		EigenVec3d p3{44552,7,0};
 		EXPECT_EQ(plane.orient(p1), cgogn::geometry::Orientation3D::OVER);
 		EXPECT_EQ(plane.orient(p2), cgogn::geometry::Orientation3D::UNDER);
 		EXPECT_EQ(plane.orient(p3), cgogn::geometry::Orientation3D::ON);
---
+++ cgogn/geometry/vec.h
@@ -0,0 +1,196 @@
+/*******************************************************************************
+* CGoGN: Combinatorial and Geometric modeling with Generic N-dimensional Maps  *
+* Copyright (C) 2015, IGG Group, ICube, University of Strasbourg, France       *
+*                                                                              *
+* This library is free software; you can redistribute it and/or modify it      *
+* under the terms of the GNU Lesser General Public License as published by the *
+* Free Software Foundation; either version 2.1 of the License, or (at your     *
+* option) any later version.                                                   *
+*                                                                              *
+* This library is distributed in the hope that it will be useful, but WITHOUT  *
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        *
+* FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License  *
+* for more details.                                                            *
+*                                                                              *
+* You should have received a copy of the GNU Lesser General Public License     *
+* along with this library; if not, write to the Free Software Foundation,      *
+* Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA.           *
+*                                                                              *
+* Web site: http://cgogn.unistra.fr/                                           *
+* Contact information: cgogn@unistra.fr                                        *
+*                                                                              *
+*******************************************************************************/
+
+#ifndef GEOMETRY_VEC_H_
+#define GEOMETRY_VEC_H_
+
+#include <array>
+#include <cmath>
+#include <initializer_list>
+
+#include <core/utils/name_types.h>
+
+#include <geometry/dll.h>
+
+namespace cgogn
+{
+
+namespace geometry
+{
+
+template<class Container>
+class Vec_T
+{
+public:
+	using Self = Vec_T<Container>;
+	using Scalar = typename std::remove_cv< typename std::remove_reference<decltype(Container()[0ul])>::type >::type;
+
+
+
+	Vec_T(const Self&) = default;
+	Vec_T(Self&&) = default;
+	Self& operator=(const Self&) = default;
+	Self& operator=(Self&&) = default;
+
+	Vec_T() = default;
+
+	inline Vec_T(const Container& cont) : data_(cont)
+	{}
+	inline Vec_T(Container&& cont) : data_(std::move(cont))
+	{}
+
+	inline Vec_T(std::initializer_list<Scalar> const & init_list)
+	{
+		auto it_this = data_.begin();
+		for (auto c : init_list)
+			*it_this++ = c;
+	}
+
+	inline Scalar& operator[](std::size_t i)
+	{
+		return data_[i];
+	}
+
+	inline const Scalar& operator[](std::size_t i) const
+	{
+		return data_[i];
+	}
+
+	inline const Self operator-() const
+	{
+		Self res(*this);
+		for(auto& c : res.data_)
+			c=-c;
+		return res;
+	}
+
+	inline void operator+=(const Self& v)
+	{
+		auto it_this = data_.begin();
+		for (auto c : v.data_)
+		{
+			*(it_this++) += c;
+		}
+	}
+
+	inline void operator-=(const Self& v)
+	{
+		auto it_this = data_.begin();
+		for (auto c : v.data_)
+		{
+			*(it_this++) -= c;
+		}
+	}
+
+	inline const Self operator+(const Self& v) const
+	{
+		Self res;
+		auto it_res = res.data_.begin();
+		auto it_v = v.data_.begin();
+		for (auto c : v.data_)
+		{
+			*(it_res++) = c + (*it_v++);
+		}
+		return res;
+	}
+
+	inline const Self operator-(const Self& v) const
+	{
+		Self res;
+		auto it_res = res.data_.begin();
+		auto it_v = v.data_.begin();
+		for (auto c : data_)
+		{
+			*(it_res++) = c - *(it_v++);
+		}
+				return res;
+	}
+
+	inline friend const Self operator*(const Self& v, Scalar r)
+	{
+		Self res(v);
+		for (auto& c : res.data_)
+			c*=r;
+		return res;
+	}
+
+	inline friend const Self operator*(Scalar r, const Self& v)
+	{
+		return v*r;
+	}
+
+	inline const Scalar dot(const Self& v) const
+	{
+		Scalar r{0};
+		auto it_v = v.data_.begin();
+		for (auto c : data_)
+			r += c* (*it_v++);
+		return r;
+	}
+
+	inline const Self cross(const Self& v) const
+	{
+		Self res;
+		res[0] = data_[1] * v[2] - data_[2] * v[1];
+		res[1] = data_[2] * v[0] - data_[0] * v[2];
+		res[2] = data_[0] * v[1] - data_[1] * v[0];
+		return res;
+	}
+
+	inline const Scalar squaredNorm() const
+	{
+		Scalar r{0};
+		for (auto c : data_)
+			r += c*c;
+		return r;
+	}
+
+	inline const Scalar norm() const
+	{
+		return std::sqrt(this->squaredNorm());
+	}
+
+	inline void normalize()
+	{
+		const Scalar norm_value = this->norm();
+		for (auto& c : data_)
+			c/= norm_value;
+	}
+
+	static std::string cgogn_name_of_type()
+	{
+		return std::string("geometry::Vec_T<") + cgogn::name_of_type(Container()) + std::string(">");
+	}
+
+private:
+	Container data_;
+};
+
+#if defined(CGOGN_USE_EXTERNAL_TEMPLATES) && (!defined(GEOMETRY_VEC_CPP_))
+extern template class CGOGN_GEOMETRY_API Vec_T<std::array<double,3>>;
+#endif // defined(CGOGN_USE_EXTERNAL_TEMPLATES) && (!defined(GEOMETRY_VEC_CPP_))
+
+} // namespace geometry
+} // namespace cgogn
+
+#endif // GEOMETRY_VEC_H_
