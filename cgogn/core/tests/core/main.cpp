06f97f8303eedb5ab9b468d14a94185b759b147d
Lionel Untereiner <lionel.untereiner@unistra.fr>
2016-01-22 15:46:27 +0100

    unit test cmap1 agin

--- cgogn/core/tests/core/main.cpp
+++ cgogn/core/tests/core/main.cpp
@@ -3,12 +3,12 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-using ::testing::AtLeast;
+// using ::testing::AtLeast;
 
 int main(int argc, char **argv)
 {
-	//testing::InitGoogleTest(&argc, argv);
-	testing::InitGoogleMock(&argc, argv);
+	testing::InitGoogleTest(&argc, argv);
+	// testing::InitGoogleMock(&argc, argv);
 	// Set LC_CTYPE according to the environnement variable.
 	setlocale(LC_CTYPE, "");
 
