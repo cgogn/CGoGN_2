87254fdbc8886a2c04ac8af83370b46815eec498
Étienne Schmitt <etienne.schmitt@inria.fr>
2016-02-02 18:05:00 +0100

    thread_pool license
    Signed-off-by: Étienne Schmitt <etienne.schmitt@inria.fr>

--- cgogn/core/utils/thread_pool.h
+++ cgogn/core/utils/thread_pool.h
@@ -21,6 +21,38 @@
 *                                                                              *
 *******************************************************************************/
 
+/*
+ * IMPORTANT : The ThreadPool code (thread_pool.h and thread_pool.cpp) is
+ * based on "A Simple c++11 threadpool implementation" found on github
+ * (https://github.com/progschj/ThreadPool, latest commit : 9a42ec1 )
+ * (c) 2012 Jakob Progsch, Václav Zeman
+ * It has been modified to fit to our purposes.
+ * A copy of its license is provided in the following lines.
+ */
+
+/****************************************************************************
+*Copyright (c) 2012 Jakob Progsch, Václav Zeman                             *
+*                                                                           *
+*This software is provided 'as-is', without any express or implied          *
+*warranty. In no event will the authors be held liable for any damages      *
+*arising from the use of this software.                                     *
+*                                                                           *
+*Permission is granted to anyone to use this software for any purpose,      *
+*including commercial applications, and to alter it and redistribute it     *
+*freely, subject to the following restrictions:                             *
+*                                                                           *
+*1. The origin of this software must not be misrepresented; you must not    *
+*claim that you wrote the original software. If you use this software       *
+*in a product, an acknowledgment in the product documentation would be      *
+*appreciated but is not required.                                           *
+*                                                                           *
+*2. Altered source versions must be plainly marked as such, and must not be *
+*misrepresented as being the original software.                             *
+*                                                                           *
+*3. This notice may not be removed or altered from any source               *
+*distribution.                                                              *
+****************************************************************************/
+
 #ifndef CORE_UTILS_THREADPOOL_H_
 #define CORE_UTILS_THREADPOOL_H_
 
