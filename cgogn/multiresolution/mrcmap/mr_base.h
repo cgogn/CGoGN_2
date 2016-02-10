ca89195a32ffb3694863d1278ba35d5f20308593
lionel untereiner <lionel.untereiner@inria.fr>
2016-02-10 09:22:01 +0100

    type correction

--- cgogn/multiresolution/mrcmap/mr_base.h
+++ cgogn/multiresolution/mrcmap/mr_base.h
@@ -21,13 +21,12 @@
 *                                                                              *
 *******************************************************************************/
 
-#ifndef MULTIRESOLUTION_MRCMAP_MRCMAP2_H_
-#define MULTIRESOLUTION_MRCMAP_MRCMAP2_H_
-
+#ifndef MULTIRESOLUTION_MRCMAP_MR_BASE_H_
+#define MULTIRESOLUTION_MRCMAP_MR_BASE_H_
 
 #include <deque>
 #include <stack>
-
+#include <vector>
 
 namespace cgogn
 {
@@ -36,135 +35,135 @@ template <typename MAP>
 class MRBase
 {
 public:
-    typedef MRBase<MAP> Self;
+	typedef MRBase<MAP> Self;
 
-    template<typename T>
-    using ChunkArray =  typename CMap2::template ChunkArray<T>;
+	template<typename T>
+	using ChunkArray =  typename MAP::template ChunkArray<T>;
 
 protected:
-        /**
-         * pointers to maps (one for each level)
-         */
-        std::deque<MAP*> maps_;
+		/**
+		 * pointers to maps (one for each level)
+		 */
+		std::deque<MAP*> maps_;
 
-        /**
-         * pointers to attributs that stores next level
-         * correspondance indices for each dart
-         */
-        std::deque<ChunkArray<unsigned int>*> next_level_indices_;
+		/**
+		 * pointers to attributs that stores next level
+		 * correspondance indices for each dart
+		 */
+		std::deque<ChunkArray<unsigned int>*> next_level_indices_;
 
-        /**
-         * pointers to attributs that stores previous level
-         * correspondance indices for each dart
-         */
-        std::deque<ChunkArray<unsigned int>*> previous_level_indices_;
+		/**
+		 * pointers to attributs that stores previous level
+		 * correspondance indices for each dart
+		 */
+		std::deque<ChunkArray<unsigned int>*> previous_level_indices_;
 
-        /**
-         * stack for current level temporary storage
-         */
-        std::stack<unsigned int, std::vector<unsigned int>> levels_stack_ ;
+		/**
+		 * stack for current level temporary storage
+		 */
+		std::stack<unsigned int, std::vector<unsigned int>> levels_stack_ ;
 
-        /**
-         * current level in multiresolution map
-         */
-        unsigned int current_level_;
+		/**
+		 * current level in multiresolution map
+		 */
+		unsigned int current_level_;
 
-        //TODO le niveau courant doit etre par thread
-        //appele sur la carte et non plus un champs de
-        //la classe carte
+		//TODO le niveau courant doit etre par thread
+		//appele sur la carte et non plus un champs de
+		//la classe carte
 
 public:
 
-        MRBase()
-        {}
+		MRBase()
+		{}
 
-        ~MRBase()
-        {}
+		~MRBase()
+		{}
 
-        MRBase(Self const&) = delete;
-        MRBase(Self &&) = delete;
-        Self& operator=(Self const&) = delete;
-        Self& operator=(Self &&) = delete;
+		MRBase(Self const&) = delete;
+		MRBase(Self &&) = delete;
+		Self& operator=(Self const&) = delete;
+		Self& operator=(Self &&) = delete;
 
-        inline void add_level_back()
-        {
-                //ajouter une carte par copie dans maps_
-                //ajouter un chunkarray dans next_
-                CMap2* last = maps_.back();
-                maps_.emplace_back(last);
-        }
+		inline void add_level_back()
+		{
+				//ajouter une carte par copie dans maps_
+				//ajouter un chunkarray dans next_
+				MAP* last = maps_.back();
+				maps_.push_back(last);
+		}
 
-        inline void remove_level_back()
-        {
-                maps_.pop_back();
-        }
+		inline void remove_level_back()
+		{
+				maps_.pop_back();
+		}
 
-        inline void add_level_front()
-        {
-                CMap2* first = maps_.front();
-                maps.emplace_front(first);
-        }
+		inline void add_level_front()
+		{
+				MAP* first = maps_.front();
+				maps.push_front(first);
+		}
 
-        inline void remove_level_front()
-        {
-                maps_.pop_front();
-        }
+		inline void remove_level_front()
+		{
+				maps_.pop_front();
+		}
 
-        //1 thread par niveau = 1 thread par carte
-        //n thread par niveau = n thread par carte
+		//1 thread par niveau = 1 thread par carte
+		//n thread par niveau = n thread par carte
 
-        inline unsigned int get_maximum_level() const
-        {
-                return static_cast<unsigned int>(maps_.size());
-        }
+		inline unsigned int get_maximum_level() const
+		{
+				return static_cast<unsigned int>(maps_.size());
+		}
 
-        inline unsigned int get_current_level() const
-        {
-                return current_level_;
-        }
+		inline unsigned int get_current_level() const
+		{
+				return current_level_;
+		}
 
-        inline void set_current_level(unsigned int l)
-        {
-                current_level_ = l;
-        }
+		inline void set_current_level(unsigned int l)
+		{
+				current_level_ = l;
+		}
 
-        inline void inc_current_level()
-        {
-                cgogn_debug_assert(get_current_level() < maps_.size() - 1, "incCurrentLevel : already at maximum resolution level");
-                ++current_level_;
-        }
+		inline void inc_current_level()
+		{
+				cgogn_debug_assert(get_current_level() < maps_.size() - 1, "incCurrentLevel : already at maximum resolution level");
+				++current_level_;
+		}
 
-        inline void dec_current_level()
-        {
-                cgogn_debug_assert(get_current_level() > 0, "decCurrentLevel : already at minimum resolution level");
-                --current_level_;
-        }
+		inline void dec_current_level()
+		{
+				cgogn_debug_assert(get_current_level() > 0, "decCurrentLevel : already at minimum resolution level");
+				--current_level_;
+		}
 
-        /**
-         * store current resolution level on a stack
-         */
-        inline void push_level()
-        {
-                levels_stack_.push_back(get_current_level()) ;
-        }
+		/**
+		 * store current resolution level on a stack
+		 */
+		inline void push_level()
+		{
+				levels_stack_.push_back(get_current_level()) ;
+		}
 
-        /**
-         * set as current the resolution level of the top of the stack
-         */
-        inline void pop_level()
-        {
-                set_current_level(levels_stack_.back()) ;
-                levels_stack_.pop_back() ;
-        }
+		/**
+		 * set as current the resolution level of the top of the stack
+		 */
+		inline void pop_level()
+		{
+				set_current_level(levels_stack_.back()) ;
+				levels_stack_.pop_back() ;
+		}
 
-        inline const MAP* current() const
-        {
-                return maps_[get_current_level()];
-        }
+		inline const MAP* current() const
+		{
+				return maps_[get_current_level()];
+		}
 
 };
 
 
 }
 
-#endif // MULTIRESOLUTION_MRCMAP_MRCMAP2_H_
+#endif // MULTIRESOLUTION_MRCMAP_MR_BASE_H_
