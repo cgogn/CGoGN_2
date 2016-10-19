b9cc5c124b133ceff8bcc6e4390167f1d2e7a61b
Étienne Schmitt <etienne.schmitt@inria.fr>
2016-10-19 17:07:28 +0200

    MapBase::add_attribute return a valid attribute when adding the same attribute a second time. If adding two attributes with same name  and same orbit but different type, it returns an invalid Attribute.
    Signed-off-by: Étienne Schmitt <etienne.schmitt@inria.fr>

--- cgogn/core/container/chunk_array_container.h
+++ cgogn/core/container/chunk_array_container.h
@@ -277,17 +277,21 @@ public:
 	{
 		cgogn_assert(name.size() != 0);
 
+		ChunkArray<T>* carr = nullptr;
+
 		// first check if attribute already exist
 		uint32 index = array_index(name);
 		if (index != UNKNOWN)
 		{
-			cgogn_log_warning("add_chunk_array") << "Chunk array of name \"" << name << "\" already exists.";
-			return nullptr;
+			carr = dynamic_cast<ChunkArray<T>*>(table_arrays_[index]);
+			if (carr == nullptr)
+				cgogn_log_warning("add_chunk_array") << "Chunk array of name \"" << name << "\" already exists with a different type \"" << table_arrays_[index]->type_name() << "\".";
+			return carr;
 		}
 
 		// create the new attribute
 		const std::string& type_name = name_of_type(T());
-		ChunkArray<T>* carr = new ChunkArray<T>(name);
+		carr = new ChunkArray<T>(name);
 		ChunkArrayFactory::template register_CA<T>();
 
 		// reserve memory
