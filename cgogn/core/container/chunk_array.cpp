44fc289229cc79e3399225c4c890e5680b1f57dc
sylvainthery <sylvain.thery@icube.unistra.fr>
2017-04-27 16:07:18 +0200

    bug fix (windows dll) external template must apply for chunk_array_factory

--- cgogn/core/container/chunk_array.cpp
+++ cgogn/core/container/chunk_array.cpp
@@ -29,9 +29,9 @@
 namespace cgogn
 {
 
-template class CGOGN_CORE_API ChunkArray<CGOGN_CHUNK_SIZE, bool>;
+//template class CGOGN_CORE_API ChunkArray<CGOGN_CHUNK_SIZE, bool>;
 template class CGOGN_CORE_API ChunkArray<CGOGN_CHUNK_SIZE, uint32>;
-template class CGOGN_CORE_API ChunkArray<CGOGN_CHUNK_SIZE, unsigned char>;
+template class CGOGN_CORE_API ChunkArray<CGOGN_CHUNK_SIZE, uint8>;
 template class CGOGN_CORE_API ChunkArray<CGOGN_CHUNK_SIZE, std::array<float32, 3>>;
 template class CGOGN_CORE_API ChunkArray<CGOGN_CHUNK_SIZE, std::array<float64, 3>>;
 template class CGOGN_CORE_API ChunkArrayBool<CGOGN_CHUNK_SIZE>;
