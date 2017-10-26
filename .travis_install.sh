22d6bca4d0cadbbbbbf40f470aa1503fb49e88cc
thery <sylvain.thery@icube.unistra.fr>
2017-10-26 23:06:59 +0200

    travis mac+linux

--- .travis_install.sh
+++ .travis_install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
- brew update
- brew install cmake
-else
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
  sudo apt-get update
  sudo apt-get install "g++-5"
  sudo apt-get install cmake3
+ sudo apt-get install libglu1-mesa-dev
 fi
 
