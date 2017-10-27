8ed5ac0981b3318c594c27e61dc2dc4fe30b003f
sylvainthery <sylvain.thery@icube.unistra.fr>
2017-10-27 16:40:57 +0200

    travis

--- .travis_install.sh
+++ .travis_install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
- brew update
- brew install cmake
-else
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
  sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
  sudo apt-get update
  sudo apt-get install "g++-5"
