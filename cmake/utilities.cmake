9046692492bc924678bdf4cd6e9e37c385485962
Lionel Untereiner <lionel.untereiner@geosiris.com>
2018-09-13 15:05:15 +0200

    modern cmake part2

--- cmake/utilities.cmake
+++ cmake/utilities.cmake
@@ -1,45 +1,6 @@
 
 include(CMakeParseArguments)
 
-
-function(add_flags _var)
-	string(REPLACE " " ";" string "${${_var}};${ARGN}")
-	list(REMOVE_DUPLICATES string)
-	string(REPLACE ";" " " string "${string}")
-	set(${_var} ${string} PARENT_SCOPE)
-endfunction()
-
-function(remove_flags _var)
-    string(REPLACE " " ";" flags "${${_var}}")
-    list(REMOVE_ITEM flags ${ARGN})
-    string(REPLACE ";" " " flags "${flags}")
-    set(${_var} ${flags} PARENT_SCOPE)
-endfunction()
-
-#!
-# @brief Add sources from directories
-# @details
-# Add the sources from the specified \p directories to variable \p var
-# and place them in the specified \p folder if non empty.
-# @param[out] var name of the variable that receives the result list
-# @param[in] folder the name of the folder
-# @param[in] directories list of directories to scan
-#
-function(aux_source_directories var folder)
-	set(sources)
-	foreach(dir ${ARGN})
-		file(GLOB _sources RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} "${dir}/*.[ch]" "${dir}/*.[ch]pp")
-		list(APPEND sources ${_sources})
-	endforeach()
-
-	if( NOT folder STREQUAL "")
-		source_group(${folder} FILES ${sources})
-	endif()
-
-	#message("\nDEBUG: aux_source_directories: current_source_dir=${CMAKE_CURRENT_SOURCE_DIR} dirs=${ARGN}):\n${sources}\n")
-	set(${var} ${${var}} ${sources} PARENT_SCOPE)
-endfunction()
-
 #!
 # @brief automatic deduction of CMAKE_BUILD_TYPE from CMAKE_CURRENT_BINARY_DIR
 # @details
@@ -80,26 +41,21 @@ endfunction(deduce_build_type)
 # cmake/<cmake-project-name>_INCLUDE_DIRS                                                    #
 ##############################################################################################
 
-macro(cgogn_create_package package_root_dir include_dirs_build_tree include_dirs_install_tree)
+macro(cgogn_create_package package_root_dir)
 
 ######## 1. Build tree
 
-set(UPPER_NAME "")
-string(TOUPPER ${PROJECT_NAME} UPPER_NAME)
-set(${UPPER_NAME}_INCLUDE_DIRS ${include_dirs_build_tree})
-
 export(TARGETS ${PROJECT_NAME}
+	NAMESPACE cgogn::
 	FILE "${CMAKE_BINARY_DIR}/lib/cmake/${PROJECT_NAME}/${PROJECT_NAME}Targets.cmake"
 )
 
-#message(${package_root_dir})
-
 configure_package_config_file(
-#	"${CGOGN_PATH}/cmake/ConfigFiles/${PROJECT_NAME}Config.cmake.in"
 	"${package_root_dir}/${PROJECT_NAME}Config.cmake.in"
 	"${CMAKE_BINARY_DIR}/lib/cmake/${PROJECT_NAME}/${PROJECT_NAME}Config.cmake"
-	PATH_VARS ${UPPER_NAME}_INCLUDE_DIRS 
 	INSTALL_DESTINATION "${CMAKE_BINARY_DIR}/lib/cmake/${PROJECT_NAME}"
+	NO_SET_AND_CHECK_MACRO
+	NO_CHECK_REQUIRED_COMPONENTS_MACRO
 )
 
 write_basic_package_version_file(
@@ -110,17 +66,16 @@ write_basic_package_version_file(
 
 ######## 2. Install tree
 
-set(${UPPER_NAME}_INCLUDE_DIRS ${include_dirs_install_tree})
-
-include(GNUInstallDirs)
-
 install(TARGETS ${PROJECT_NAME}
 	EXPORT ${PROJECT_NAME}Targets
 	RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
 	LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
 	ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
 )
-install(EXPORT ${PROJECT_NAME}Targets DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}")
+
+install(EXPORT ${PROJECT_NAME}Targets
+		NAMESPACE cgogn::
+		DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}")
 
 ## <package_name>ConfigVersion.cmake
 write_basic_package_version_file(
@@ -132,11 +87,11 @@ write_basic_package_version_file(
 ## <package_name>Config.cmake
 set(CURRENT_LIBRARY "${PROJECT_NAME}")
 configure_package_config_file(
-#	"${CGOGN_PATH}/cmake/ConfigFiles/${PROJECT_NAME}Config.cmake.in"
 	"${package_root_dir}/${PROJECT_NAME}Config.cmake.in"
 	"${CMAKE_BINARY_DIR}/cmake/${PROJECT_NAME}/${PROJECT_NAME}InstallConfig.cmake"
-	PATH_VARS ${UPPER_NAME}_INCLUDE_DIRS
-	INSTALL_DESTINATION "lib/cmake/${PROJECT_NAME}"
+	INSTALL_DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}"
+	NO_SET_AND_CHECK_MACRO
+	NO_CHECK_REQUIRED_COMPONENTS_MACRO
 )
 
 install(FILES "${CMAKE_BINARY_DIR}/cmake/${PROJECT_NAME}/${PROJECT_NAME}ConfigVersion.cmake" DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}")
@@ -147,13 +102,13 @@ endmacro()
 
 ##############################################################################################
 
-macro(subdirlist result curdir)
-  file(GLOB children RELATIVE ${curdir} ${curdir}/*)
+function(cgogn_list_subdirectory result current_directory)
+  file(GLOB children RELATIVE ${current_directory} ${current_directory}/*)
   set(dirlist "")
   foreach(child ${children})
-    if(IS_DIRECTORY ${curdir}/${child})
+	if(IS_DIRECTORY ${current_directory}/${child})
 		list(APPEND dirlist ${child})
     endif()
   endforeach()
-  set(${result} ${dirlist})
-endmacro()
+  set(${result} ${dirlist} PARENT_SCOPE)
+endfunction()
