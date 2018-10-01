f6209d7c863b9a6aed9a273d68e949cca246404e
Lionel Untereiner <lionel.untereiner@geosiris.com>
2018-10-01 17:12:24 +0200

    update CI scripts

--- cmake/Modules/FindTinyXML2.cmake
+++ cmake/Modules/FindTinyXML2.cmake
@@ -25,11 +25,22 @@ find_path(TinyXML2_INCLUDE_DIR
 	DOC "The TinyXML2 include directory"
 )
 
-find_library(TinyXML2_LIBRARY
-	NAMES tinyxml2
-	HINTS ${PC_TinyXML2_LIBDIR} ${PC_TinyXML2_LIBRARY_DIRS}
-	DOC "The TinyXML2 library"
-)
+if(NOT TinyXML2_LIBRARY)
+	find_library(TinyXML2_LIBRARY_RELEASE
+		NAMES tinyxml2
+		HINTS ${PC_TinyXML2_LIBDIR} ${PC_TinyXML2_LIBRARY_DIRS}
+		DOC "The release TinyXML2 library"
+	)
+
+	find_library(TinyXML2_LIBRARY_DEBUG
+		NAMES tinyxml2d
+		HINTS ${PC_TinyXML2_LIBDIR} ${PC_TinyXML2_LIBRARY_DIRS}
+		DOC "The debug TinyXML2 library"
+	)
+endif()
+
+include(SelectLibraryConfigurations)
+select_library_configurations(TinyXML2)
 
 set(TinyXML2_VERSION ${PC_TinyXML2_VERSION})
 
@@ -46,7 +57,20 @@ if(TinyXML2_FOUND)
 	if(NOT TARGET TinyXML2::TinyXML2)
 	    add_library(TinyXML2::TinyXML2 UNKNOWN IMPORTED)
 	    set_target_properties(TinyXML2::TinyXML2 PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${TinyXML2_INCLUDE_DIR}")
-	    set_property(TARGET TinyXML2::TinyXML2 APPEND PROPERTY IMPORTED_LOCATION "${TinyXML2_LIBRARY}")
+
+	    if(TinyXML2_LIBRARY_RELEASE)
+        	set_property(TARGET TinyXML2::TinyXML2 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+        	set_target_properties(TinyXML2::TinyXML2 PROPERTIES IMPORTED_LOCATION_RELEASE "${TinyXML2_LIBRARY_RELEASE}")
+		endif()
+
+		if(TinyXML2_LIBRARY_DEBUG)
+        	set_property(TARGET TinyXML2::TinyXML2 APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+        	set_target_properties(TinyXML2::TinyXML2 PROPERTIES IMPORTED_LOCATION_DEBUG "${TinyXML2_LIBRARY_DEBUG}")
+		endif()
+
+		if(NOT TinyXML2_LIBRARY_RELEASE AND NOT TinyXML2_LIBRARY_DEBUG)
+        	set_property(TARGET TinyXML2::TinyXML2 APPEND PROPERTY IMPORTED_LOCATION "${TinyXML2_LIBRARY}")
+		endif()
 	endif()
 endif()
 
